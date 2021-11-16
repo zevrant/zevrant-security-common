a82ff7ac5b12478671f3c26c81b1c25dc56a8f3f
zevrant <gerethd@gmail.com>
2021-11-16 17:18:00 -0500

    added missing @configuration annotation

--- src/main/java/com/zevrant/services/zevrantsecuritycommon/config/RestTemplateConfig.java
+++ src/main/java/com/zevrant/services/zevrantsecuritycommon/config/RestTemplateConfig.java
@@ -2,8 +2,10 @@ package com.zevrant.services.zevrantsecuritycommon.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
 public class RestTemplateConfig {
 
   @Bean
