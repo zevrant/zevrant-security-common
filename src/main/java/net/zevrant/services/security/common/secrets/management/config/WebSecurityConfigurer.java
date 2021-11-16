e753a0f1ac5d5e0733b61c67fb29b7e12119ffd9
zevrant <gerethd@gmail.com>
2021-11-15 21:53:06 -0500

    removed spring annotations

--- src/main/java/net/zevrant/services/security/common/secrets/management/config/WebSecurityConfigurer.java
+++ src/main/java/net/zevrant/services/security/common/secrets/management/config/WebSecurityConfigurer.java
@@ -1,40 +1,40 @@
-package net.zevrant.services.security.common.secrets.management.config;
-
-import net.zevrant.services.security.common.secrets.management.filter.OAuthSecurityFilter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
-import org.springframework.web.client.RestTemplate;
-
-public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
-
-    private final String baseUrl;
-    private final RestTemplate restTemplate;
-    private final AuthenticationManager authenticationManager;
-    private final String[] unsecuredEndpoints;
-
-    public WebSecurityConfigurer(@Value("${zevrant.services.proxy.baseUrl}") String baseUrl, RestTemplate restTemplate,
-                                 AuthenticationManager authenticationManager,
-                                 @Value("${zevrant.unsecured.endpoints:/actuator/health,/actuator/info,/actuator/prometheus}")
-                                         String[] unsecuredEndpoints) {
-        this.baseUrl = baseUrl;
-        this.restTemplate = restTemplate;
-        this.authenticationManager = authenticationManager;
-        this.unsecuredEndpoints = unsecuredEndpoints;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .anonymous().and()
-                .authorizeRequests().antMatchers(unsecuredEndpoints).permitAll()
-                .and()
-                .csrf().disable()
-                .httpBasic().disable()
-                .addFilterBefore(new OAuthSecurityFilter(this.baseUrl, this.restTemplate, this.authenticationManager), AnonymousAuthenticationFilter.class);
-        super.configure(http);
-    }
-
-}
+//package net.zevrant.services.security.common.secrets.management.config;
+//
+//import net.zevrant.services.security.common.secrets.management.filter.OAuthSecurityFilter;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+//import org.springframework.web.client.RestTemplate;
+//
+//public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+//
+//    private final String baseUrl;
+//    private final RestTemplate restTemplate;
+//    private final AuthenticationManager authenticationManager;
+//    private final String[] unsecuredEndpoints;
+//
+//    public WebSecurityConfigurer(@Value("${zevrant.services.proxy.baseUrl}") String baseUrl, RestTemplate restTemplate,
+//                                 AuthenticationManager authenticationManager,
+//                                 @Value("${zevrant.unsecured.endpoints:/actuator/health,/actuator/info,/actuator/prometheus}")
+//                                         String[] unsecuredEndpoints) {
+//        this.baseUrl = baseUrl;
+//        this.restTemplate = restTemplate;
+//        this.authenticationManager = authenticationManager;
+//        this.unsecuredEndpoints = unsecuredEndpoints;
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .anonymous().and()
+//                .authorizeRequests().antMatchers(unsecuredEndpoints).permitAll()
+//                .and()
+//                .csrf().disable()
+//                .httpBasic().disable()
+//                .addFilterBefore(new OAuthSecurityFilter(this.baseUrl, this.restTemplate, this.authenticationManager), AnonymousAuthenticationFilter.class);
+//        super.configure(http);
+//    }
+//
+//}
--- src/main/java/net/zevrant/services/security/common/secrets/management/config/ZevrantAuthenticationManager.java
+++ src/main/java/net/zevrant/services/security/common/secrets/management/config/ZevrantAuthenticationManager.java
@@ -3,9 +3,7 @@ package net.zevrant.services.security.common.secrets.management.config;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.stereotype.Component;
 
-@Component
 public class ZevrantAuthenticationManager implements AuthenticationManager {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
